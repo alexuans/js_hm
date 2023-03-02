99078da700ab98d30d1a78f4046368a598bb7ec1
Olexiy Grigoryak <alexuans@gmail.com>
2023-03-02 22:14:29 +0200

    hm21

---
+++ hm21/script.js
@@ -0,0 +1,6 @@
+let num1 = prompt('Enter first number:', 'enter a number');
+let num2 = prompt('Enter second number:', 'enter a number');
+alert(`Sum = ${Number(num1) + Number(num2)},
+Difference = ${Number(num1) - Number(num2)},
+Multiplication = ${Number(num1) * Number(num2)},
+Division = ${Number(num1) / Number(num2)}.`);
